f0517e5e81051714a26ce2bec276c738f59b78f8
Nikita <nikita.zelentzoff@yandex.ru>
2020-03-31 19:45:51 +0300

    Result

---
+++ Wombat/Wombat/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Wombat
+{
+    abstract class Entity
+    {
+        public int health;
+
+        public void TakeDamage(int damage)
+        {
+            ApplyDamage(damage);
+
+            if (health <= 0)
+                Console.WriteLine("I'm died");
+        }
+
+        protected abstract void ApplyDamage(int damage);
+    }
+
+    class Wombat : Entity
+    {
+        public int armor;
+
+        protected override void ApplyDamage(int damage) =>
+            health -= damage - armor;
+    }
+
+    class Human : Entity
+    {
+        public int agility;
+
+        protected override void ApplyDamage(int damage) =>
+            health -= damage / agility;
+    }
+}
