bbe2efb33bb29b9adb4b6ff6fdbf8878f359efc7
Nikita <nikita.zelentzoff@yandex.ru>
2020-03-31 19:42:28 +0300

    Result

---
+++ DataDefence/DataDefence/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataDefence
+{
+    class Bag
+    {
+        public IEnumerable<IReadOnlyItem> Items => _items;
+        public readonly uint maxWeidth;
+        private readonly List<Item> _items = new List<Item>();
+
+        public Bag(uint maxWeidth) =>
+            this.maxWeidth = maxWeidth;
+
+        public void AddItem(string name, int count)
+        {
+            int currentWeidth = _items.Sum(item => item.count);
+            Item targetItem = _items.FirstOrDefault(item => item.name == name);
+
+            _ = targetItem
+                ?? throw new InvalidOperationException();
+
+            targetItem.count = currentWeidth + count > maxWeidth
+                ? throw new InvalidOperationException()
+                : targetItem.count + count;
+        }
+    }
+
+    class Item : IReadOnlyItem
+    {
+        public readonly string name;
+        public int count;
+
+        public Item(int count, string name) =>
+            (this.name, this.count) = (name, count);
+
+        int IReadOnlyItem.Count => count;
+        string IReadOnlyItem.Name => name;
+    }
+
+    interface IReadOnlyItem
+    {
+        int Count { get; }
+        string Name { get; }
+    }
+}
