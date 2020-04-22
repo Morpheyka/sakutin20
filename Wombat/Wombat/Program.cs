bfd58e77d2ce49a58d9893f6d91c86194ff2b7c2
Nikita <38893125+Morpheyka@users.noreply.github.com>
2020-04-22 17:12:28 +0300

    Update Program.cs

--- Wombat/Wombat/Program.cs
+++ Wombat/Wombat/Program.cs
@@ -4,32 +4,32 @@ namespace Wombat
 {
     abstract class Unit
     {
-        protected int health;
+        private int _health;
 
         public void TakeDamage(int damage)
         {
-            ApplyDamage(damage);
+            _health -= ApplyDamage(damage);
 
-            if (health <= 0)
+            if (_health <= 0)
                 Console.WriteLine("I'm died");
         }
 
-        protected abstract void ApplyDamage(int damage);
+        protected abstract int ApplyDamage(int damage);
     }
 
     class Wombat : Unit
     {
         private int _armor;
 
-        protected override void ApplyDamage(int damage) =>
-            health -= damage - _armor;
+        protected override int ApplyDamage(int damage) =>
+            damage - _armor;
     }
 
     class Human : Unit
     {
         private int _agility;
 
-        protected override void ApplyDamage(int damage) =>
-            health -= damage / _agility;
+        protected override int ApplyDamage(int damage) =>
+            damage / _agility;
     }
 }
